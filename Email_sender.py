992eba647d096a1780cc5d2071cb79f534668915
AleksandarBuk <aleksandar.buk@gmail.com>
2023-08-20 23:30:37 +0200

    Update

--- Email_sender.py
+++ Email_sender.py
@@ -16,6 +16,9 @@ email.set_content(html.substitute('Crash news'), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('INPUT_RECEIVERS_E-MAIL', 'INPUT_YOUR_GOOGLE_SECURITY_PASSWORD') # use google's Securrity app password to allow the app to send the e-mail message
+    smtp.login('INPUT_RECEIVERS_E-MAIL',
+               'INPUT_YOUR_GOOGLE_SECURITY_PASSWORD')  # use google's Securrity app password to allow the app to send the e-mail message
     smtp.send_message(email)
     print('Successfully sent')
+
+# sending to e-mail lists functionality
