9656d722c8a86b519be15b8015347d36d442a54a
AleksandarBuk <aleksandar.buk@gmail.com>
2023-08-21 23:18:10 +0200

    Class User declaration

--- Email_sender.py
+++ Email_sender.py
@@ -3,6 +3,7 @@ from email.message import EmailMessage
 from string import Template
 from pathlib import Path
 
+
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'INPUT_SENDERS_NAME'
@@ -10,6 +11,20 @@ email['to'] = 'INPUT_RECEIVERS_E-MAIL'
 email['subject'] = 'Summer is here!'
 
 email.set_content(html.substitute('Crash news'), 'html')
+
+#Define class user for registration
+class User:
+
+    def __init__(self, name, signature):
+        self.name = name
+        self.signature = signature
+
+#basic function
+    def Greeting(self):
+        print(self.name)
+
+    def Yell(self):
+        print(f'{self.name} has logged in. {self.signature}')
 
 # uses SMTP server to log into gmail client and send the email
 
